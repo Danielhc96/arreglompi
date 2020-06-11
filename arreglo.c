9493d7f6146be271068a6c403b9cdff781287432
Danielhc96 <45342504+Danielhc96@users.noreply.github.com>
2020-06-11 19:02:40 -0400

    Update arreglo.c

--- arreglo.c
+++ arreglo.c
@@ -4,6 +4,7 @@
 
 int  main ( int argc, char * argv [])
 {
+  int i;
   int arreglo[10];
   for (i=0; i<10; i++){
     arreglo[i] = i;
@@ -14,8 +15,8 @@ int  main ( int argc, char * argv [])
   MPI_Comm_size(MPI_COMM_WORLD, &npr);
   MPI_Comm_rank(MPI_COMM_WORLD,&pid);
   
-  int p = n/npr;          /*parte a repartir*/
-  int r = n%npr;          /*numeros sobrantes (resto)*/
+  int p = 10/npr;          /*parte a repartir*/
+  int r = 10%npr;          /*numeros sobrantes (resto)*/
   int nm;
   int tag1 = 2;
   int tag2 = 1;
@@ -35,7 +36,7 @@ int  main ( int argc, char * argv [])
      for (j=0;j<p;j++){
       printf("\nNumero %d = %d", j+1, arreglo[j]);
      }
-     for (i=1; i<numtasks; i++) {
+     for (i=1; i<npr; i++) {
       source = i;
       MPI_Recv(&nm, 1, MPI_INT, source, tag1, MPI_COMM_WORLD, &status);
       MPI_Recv(&arreglo[nm], p, MPI_INT, source, tag2, MPI_COMM_WORLD, &status);
