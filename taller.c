63e3992f1c5d31542935b9f6770bc35af5ded85b
Danielhc96 <45342504+Danielhc96@users.noreply.github.com>
2020-06-12 00:15:10 -0400

    Update taller.c

--- taller.c
+++ taller.c
@@ -125,22 +125,6 @@ int main (int argc, char *argv[]){
             printf("\nNumero %d = %d", i+1, arreglo[i]);
         }
         printf("\n\n");
-        
-        nm = p+r; 
-        printf("\n\nArreglo en maestro");
-        for (i=0; i<nm; i++){
-            printf("\nNumero %d = %d", i+1, arreglo[i]);
-        } 
-        printf("\n\nArreglo en nodo");
-        for(i=1; i<npr; i++){
-                for (j=nm; j<nm+p; j++){
-                arreglo3[j-nm]=arreglo[j];
-                printf("\nNumero %d = %d", j+1, arreglo3[j-nm]);
-            }
-            printf("\n");
-            nm = nm + p;
-        }
-  
     }
   
     /*********
