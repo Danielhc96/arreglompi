c4ea37a500cadfcb521232880714a540dd404aa7
Danielhc96 <45342504+Danielhc96@users.noreply.github.com>
2020-06-12 03:02:13 -0400

    Update taller.c

--- taller.c
+++ taller.c
@@ -63,7 +63,51 @@ void merge(int a[], int m, int b[], int n, int sorted[]) {
     }
   }
 }
-
+void merge2(int arr[], int l, int m, int r)
+{
+int i, j, k;
+int n1 = m - l + 1;
+int n2 = r - m;
+// Create temp arrays
+int L[n1], R[n2];
+// Copy data to temp array
+for (i = 0; i < n1; i++)
+L[i] = arr[l + i];
+for (j = 0; j < n2; j++)
+R[j] = arr[m + 1+ j];
+// Merge the temp arrays
+i = 0;
+j = 0;
+k = l;
+while (i < n1 && j < n2)
+{
+if (L[i] <= R[j])
+{
+arr[k] = L[i];
+i++;
+}
+else
+{
+arr[k] = R[j];
+j++;
+}
+k++;
+}
+// Copy the remaining elements of L[]
+while (i < n1)
+{
+arr[k] = L[i];
+i++;
+k++;
+}
+// Copy the remaining elements of R[]
+while (j < n2)
+{
+arr[k] = R[j];
+j++;
+k++;
+}
+}
 int main (int argc, char *argv[]){
   
     /*********
@@ -179,7 +223,8 @@ int main (int argc, char *argv[]){
             for(j=0; j<p; j++){
                 arreglo3[j] = arreglo[j+nm];
             }
-            merge(arreglo2, nm, arreglo3, p, arreglo4);
+            merge2(arreglo,0,nm,nm+p);
+            /*merge(arreglo2, nm, arreglo3, p, arreglo4);*/
             nm = nm + p;
         }
       
